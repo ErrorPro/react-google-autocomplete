d14480c21b5ce698a1fddf4eab0663e8dfd9d992
Ven Korolev <mohtaiiihuk@gmail.com>
2016-05-22 20:13:25 +0700

    WIP: added custom component

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 import React, {PropTypes} from 'react';
 
-export default class ReactGoogleAutocomplete extends React.Component {
+export class ReactGoogleAutocomplete extends React.Component {
   static propTypes = {
     onPlaceSelected: PropTypes.func,
   }
@@ -31,6 +31,62 @@ export default class ReactGoogleAutocomplete extends React.Component {
           ref="input"
           {...this.props}
         />
+      </div>
+    );
+  }
+}
+
+export class ReactCustomGoogleAutocomplete extends React.Component {
+  static propTypes = {
+    input: PropTypes.node.isRequired,
+    onOpen: PropTypes.func.isRequired,
+    onClose: PropTypes.func.isRequired,
+  }
+
+  constructor(props) {
+    super(props);
+    this.service = new google.maps.places.AutocompleteService();
+  }
+
+	onChange(e) {
+    if(e.target.value) {
+      this.service.getPlacePredictions({input: e.target.value, types: ['(cities)'],}, (predictions, status) => {
+        if (status === 'OK' && predictions && predictions.length > 0) {
+          this.props.onOpen(predictions);
+            console.log(predictions);
+        } else {
+          this.props.onClose();
+        }
+      });
+    } else {
+      this.props.onClose();
+    }
+  }
+
+	componentDidMount() {
+    if (this.props.input.value) {
+      this.placeService = new google.maps.places.PlacesService(this.refs.div);
+      this.placeService.getDetails({placeId: this.props.input.value}, (e, status) => {
+        if(status === 'OK') {
+					this.refs.input.value = e.formatted_address;
+        }
+      });
+    }
+  }
+
+  render() {
+    return (
+      <div>
+        {React.cloneElement(this.props.input,
+          {
+            ...this.props,
+    			  ref: 'input',
+            onChange: (e) => {
+              this.onChange(e);
+            },
+          }
+        )}
+        <div ref="div"></div>
       </div>
     );
   }
