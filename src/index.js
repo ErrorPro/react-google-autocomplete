b05e0e0c3aaea9422468b0662e19270130d0be15
Math.random() <math.valeriano@gmail.com>
2020-05-03 15:28:59 -0300

    feat: add options to autocomplete constructor
    option to be used in autocomplete constructor.
    see more about [here](https://developers.google.com/maps/documentation/javascript/places-autocomplete#add_autocomplete)
    
    resolves #72

--- src/index.js
+++ src/index.js
@@ -4,11 +4,21 @@ import PropTypes from 'prop-types';
 export default class ReactGoogleAutocomplete extends React.Component {
   static propTypes = {
     onPlaceSelected: PropTypes.func,
-    types: PropTypes.array,
+    types: PropTypes.arrayOf(PropTypes.string),
     componentRestrictions: PropTypes.object,
     bounds: PropTypes.object,
     fields: PropTypes.array,
     inputAutocompleteValue: PropTypes.string,
+    options: PropTypes.shape({
+      componentRestrictions: PropTypes.object,
+      bounds: PropTypes.object,
+      location: PropTypes.object,
+      offset: PropTypes.number,
+      origin: PropTypes.object,
+      radius: PropTypes.number,
+      sessionToken: PropTypes.object,
+      types: PropTypes.arrayOf(PropTypes.string)
+    }),
   };
 
   constructor(props) {
@@ -27,9 +37,11 @@ export default class ReactGoogleAutocomplete extends React.Component {
         'geometry.location',
         'place_id',
         'formatted_address'
-      ]
+      ],
+      options = {}
     } = this.props;
     const config = {
+      ...options,
       types,
       bounds,
       fields
@@ -84,6 +96,7 @@ export default class ReactGoogleAutocomplete extends React.Component {
       types,
       componentRestrictions,
       bounds,
+      options,
       ...rest
     } = this.props;
 
